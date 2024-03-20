f8636563945f231348046610ad2485a8c279da69
woogym <kwj9294@gmail.com>
2024-03-20 15:21:47 +0900

    다시 수정

--- server_build_test/src/Main.java
+++ server_build_test/src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        
-        String message = "안녕하세요";
-
-        public void say() {
-            System.out.println(message);
-        }
-        
+        System.out.println("안녕하세요");
     }
 }
