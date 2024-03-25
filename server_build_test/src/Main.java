4a8ec7f21a0a1f9c122253be91ce91c77ac3a876
woogym <kwj9294@gmail.com>
2024-03-25 15:16:23 +0900

    Hello 클래스에 message변수 추가

--- server_build_test/src/Main.java
+++ server_build_test/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        String message = "안녕하세요";
+
+        public void say() {
+            System.out.println(message);
+        }
+        
     }
 }
